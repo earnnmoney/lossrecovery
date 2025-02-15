06c08bdeced7a2c0084170c70dd371792c345505
earnnmoney <mdamannullahweb@gmail.com>
2025-02-15 08:40:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -111,3 +111,22 @@ window.addEventListener("resize", function () {
 
   moveSliderItem();
 });
+
+
+
+
+
+//alert 
+
+
+ const popup = document.querySelector('.popup');
+        const x = document.querySelector('.popup-content h1')
+
+        window.addEventListener('load', () => {
+            popup.classList.add('showPopup');
+            popup.childNodes[1].classList.add('showPopup');
+        })
+        x.addEventListener('click', () => {
+            popup.classList.remove('showPopup');
+            popup.childNodes[1].classList.remove('showPopup');
+        })
